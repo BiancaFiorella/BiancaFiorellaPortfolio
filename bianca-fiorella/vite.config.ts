a08bfcb2331819eb853e053bd787be602f2a0023
Bianca Fiorella <86573050+BiancaFiorella@users.noreply.github.com>
2025-02-13 01:27:20 -0600

    trying to fix pipeline

--- bianca-fiorella/vite.config.ts
+++ bianca-fiorella/vite.config.ts
@@ -3,11 +3,21 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig((configEnv) => {
   const dotnetVersion = "net9.0";
+
+  const isCi = configEnv.mode === "ci";
+
+  if (isCi) {
+    configEnv.mode = "production"
+  }
+
   const isProduction = configEnv.mode === "production";
   const dotnetFolder = isProduction ? "Release" : "Debug";
+
+  const out = (isCi ? process.env.VITE_OUTDIR : `../BiancaFiorellaPortfolio/bin/${dotnetFolder}/${dotnetVersion}`) + '/dist';
+
   return {
     build: {
-      outDir: `../BiancaFiorellaPortfolio/bin/${dotnetFolder}/${dotnetVersion}/${(isProduction ? 'publish/' : '')}dist`,
+      outDir: out,
       emptyOutDir: true,
       copyPublicDir: true,
     },
